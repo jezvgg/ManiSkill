Fix evaluate, add success rate calculation and headless mode for planner

mani_skill/envs/tasks/tabletop/my_robocasa.py
@@ -137,11 +137,21 @@ class MyRoboCasaScene(BaseEnv):
         центральную позицию чашки. По идее это значит, что кружка в чашке.
         """
         bowl_radius = get_actor_size(self.bowl)[0] / 2
-        xy_distance = (
-            torch.linalg.norm(self.bowl.pose.p - self.cup.pose.p, dim=1) <= bowl_radius
-        )
-        z_distance = self.bowl.pose.p[0][2] >= self.cup.pose.p[0][2]
-        return dict(success=xy_distance & z_distance)
+        
+        bowl_pos = self.bowl.pose.p
+        cup_pos = self.cup.pose.p
+
+        xy_diff = torch.linalg.norm(bowl_pos[..., :2] - cup_pos[..., :2], dim=1)
+        z_diff = cup_pos[..., 2] - bowl_pos[..., 2]
+        
+        is_grasped = self.agent.is_grasping(self.cup)
+
+        xy_distance = xy_diff <= bowl_radius
+        z_distance = (z_diff >= 0.02) & (z_diff <= 0.08)
+        
+        success = xy_distance & z_distance & (~is_grasped)
+
+        return dict(success=success)
 
     def compute_dense_reward(self):
         """

planner.py
@@ -18,7 +18,9 @@ from mani_skill.examples.motionplanning.fetch.utils import (
 from mani_skill.utils.wrappers.record import RecordEpisode
 
 
-def planning(env, seed, debug=False) -> bool:
+def planning(env, seed, debug=False, vis=None) -> bool:
+    if vis is None:
+        vis = (env.unwrapped.render_mode == "human")
     unwenv: MyRoboCasaScene = env.unwrapped
     agent: Fetch = unwenv.agent
     FINGER_LENGTH = 0.025
@@ -26,7 +28,7 @@ def planning(env, seed, debug=False) -> bool:
     planner = FetchMotionPlanningSapienSolver(
         env,
         base_pose=agent.robot.pose,
-        vis=True,
+        vis=vis,
         print_env_info=True,
         debug=debug,
     )
@@ -157,7 +159,7 @@ def planning(env, seed, debug=False) -> bool:
         action = np.hstack([arm_action, gripper_action, body_action, base_action])
         obs, reward, terminated, truncated, info = env.step(action)
 
-        if hasattr(unw_env, "render_human"):
+        if vis and hasattr(unw_env, "render_human"):
             unw_env.render_human()
 
     # Синхронизируем планер
@@ -174,7 +176,7 @@ def planning(env, seed, debug=False) -> bool:
 
     for _ in range(40):
         env.step(action)
-        if hasattr(unw_env, "render_human"):
+        if vis and hasattr(unw_env, "render_human"):
             unw_env.render_human()
 
     planner.planner.update_from_simulation()
@@ -209,5 +211,5 @@ if __name__ == "__main__":
         save_on_reset=True,
     )
     env.action_space.seed(SEED)
-    planning(env)
+    planning(env, SEED)
     env.close()